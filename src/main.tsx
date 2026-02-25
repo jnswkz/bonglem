ae5335f32d6a5ec5ae8cb4911dec0a550940c54e
jnswkz <23520408@gm.uit.edu.vn>
2026-02-25 12:41:31 +0700

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import App from "./App";
 import "./styles/globals.css";
 import { LanguageProvider } from "./i18n/LanguageContext";
 import { CartProvider } from "./store/CartContext";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <LanguageProvider>
       <CartProvider>
         <App />
+        <Analytics />
       </CartProvider>
     </LanguageProvider>
   </React.StrictMode>
 );
-
